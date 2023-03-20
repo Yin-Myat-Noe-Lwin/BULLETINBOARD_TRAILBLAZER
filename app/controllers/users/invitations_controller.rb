074e5a9e10c4f98ff3506f4d3c33df6b6ff695c1
lwinleo05 <yinmyatnoelwin1@gmail.com>
2023-03-20 08:09:58 +0630

    invitations_controller fixed

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -5,15 +5,15 @@ class InvitationsController < Devise::InvitationsController
     end
   
     def create
-      User.invite!(invite_params, current_user)
-        redirect_to users_path
+      super
     end
-  
+
+    def edit 
+      super
+    end
+
     def update
-      user = User.accept_invitation!(accept_invitation_params)
-    end
-  
-    def edit
+      super
     end
   
   end
